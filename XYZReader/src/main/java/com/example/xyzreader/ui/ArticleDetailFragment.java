5f5bd1eaf52dd72fdc1c3b137762082868be8554
kyodgorbek <kyodgorbek@gmail.com>
2018-02-08 12:33:45 +0100

    I have implemented android:elavation in ToolBar I have done as you told me please check my updated code thanks

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -187,8 +187,8 @@ public class ArticleDetailFragment extends Fragment implements
         bylineView.setMovementMethod(new LinkMovementMethod());
         TextView bodyView = (TextView) mRootView.findViewById(R.id.article_body);
         bodyView.setTypeface(Typeface.createFromAsset(getResources().getAssets(), "Rosario-Regular.ttf"));
-        titleView.setTypeface(Typeface.createFromAsset(getResources().getAssets(), "RobotoBold-Italic.ttf"));
-        bylineView.setTypeface(Typeface.createFromAsset(getResources().getAssets(), "RobotoBold-Italic.ttf"));
+        titleView.setTypeface(Typeface.createFromAsset(getResources().getAssets(), "Roboto-BoldItalic.ttf"));
+        bylineView.setTypeface(Typeface.createFromAsset(getResources().getAssets(), "Roboto-BoldItalic.ttf"));
 
         if (mCursor != null) {
             mRootView.setAlpha(0);
